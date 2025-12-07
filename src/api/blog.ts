67ae351463525d7c452c2d272aaba60982e5caa7
nmhnmhnmh812 <nmhnmh812@gmail.com>
2025-12-07 22:25:01 +0700

    feat: Enhance blog functionality with filtering options and add blog categories endpoint

--- src/api/blog.ts
+++ src/api/blog.ts
@@ -3,10 +3,22 @@ import baseAxios from ".";
 export const ENDPOINTS = {
   BLOGS: "/blogs",
   BLOG_DETAIL: (blogId: number) => `/blogs/${blogId}`,
+  BLOG_CATEGORIES: "/blog-categories",
 };
 
-export async function getBlogs(params: { page: number; size: number }) {
+export async function getBlogs(params: {
+  page: number;
+  size: number;
+  keyword?: string;
+  categoryId?: number;
+  fromDate?: string;
+  toDate?: string;
+}) {
   return await baseAxios.get(ENDPOINTS.BLOGS, { params });
+}
+
+export async function getBlogCategories() {
+  return await baseAxios.get(ENDPOINTS.BLOG_CATEGORIES);
 }
 
 export async function getBlogBySlug(blogId: number) {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -36,3 +36,50 @@ export const calculateDiscountPercent = (
   if (originalPrice <= 0 || discountAmount <= 0) return 0;
   return Math.round((discountAmount / originalPrice) * 100);
 };
+
+import DOMPurify from "dompurify";
+
+export const sanitizedDescription = (content: string) => {
+  if (typeof window === "undefined") {
+    // Server-side: return raw HTML (will be sanitized on client)
+    return "";
+  }
+  // Client-side: sanitize HTML
+  return DOMPurify.sanitize(content || "", {
+    ALLOWED_TAGS: [
+      "p",
+      "br",
+      "strong",
+      "em",
+      "u",
+      "h1",
+      "h2",
+      "h3",
+      "h4",
+      "h5",
+      "h6",
+      "ul",
+      "ol",
+      "li",
+      "a",
+      "img",
+      "div",
+      "span",
+      "table",
+      "tr",
+      "td",
+      "th",
+      "thead",
+      "tbody",
+      "blockquote",
+      "code",
+      "pre",
+    ],
+    ALLOWED_ATTR: ["href", "src", "alt", "title", "class", "style", "target"],
+    ALLOW_DATA_ATTR: false,
+    ADD_TAGS: ["iframe"], 
+    ADD_ATTR: ["allow", "allowfullscreen", "frameborder", "scrolling"], 
+    ALLOWED_URI_REGEXP:
+      /^(?:(?:(?:f|ht)tps?|mailto|tel|callto|sms|cid|xmpp):|[^a-z]|[a-z+.\-]+(?:[^a-z+.\-:]|$))/i,
+  });
+};
