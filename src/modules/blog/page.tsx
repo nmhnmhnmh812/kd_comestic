85f8199e2dc4e4c085d38b407c3379004a56f46f
nmhnmhnmh812 <nmhnmh812@gmail.com>
2025-12-29 21:15:08 +0700

    feat: refactor blog rendering by creating a reusable BlogItem component

--- src/modules/blog/page.tsx
+++ src/modules/blog/page.tsx
@@ -18,6 +18,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { convertToUrl, sanitizedDescription } from "@/utils";
 import dayjs, { Dayjs } from "dayjs";
+import BlogItem from "@/components/BlogItem";
 
 const { RangePicker } = DatePicker;
 
@@ -90,47 +91,7 @@ export default function BlogScreen() {
 
   const renderBlogs = blogs?.length ? (
     blogs.map((blog) => {
-      const blogUrl = convertToUrl(blog.title, blog.id);
-      return (
-        <Link
-          key={blog.id}
-          href={`/blog/${blogUrl}`}
-          className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300"
-        >
-          <div className="relative w-full aspect-video">
-            <Image
-              src={blog.thumbnail}
-              alt={blog.title}
-              fill
-              className="object-cover"
-            />
-          </div>
-          <div className="p-4">
-            <div className="flex items-center justify-between gap-2 mb-2">
-              <span className="text-xs bg-blue-100 text-blue-600 px-2 py-1 rounded">
-                {blog.categoryName}
-              </span>
-              <p className="text-sm text-gray-400">
-                {new Date(blog.createdDate).toLocaleDateString("vi-VN", {
-                  year: "numeric",
-                  month: "long",
-                  day: "numeric",
-                })}
-              </p>
-            </div>
-            <h2 className="text-lg font-bold mb-2 line-clamp-2">
-              {blog.title}
-            </h2>
-
-            <p
-              className="ql-editor text-gray-600 h-[66px] line-clamp-3 p-0"
-              dangerouslySetInnerHTML={{
-                __html: sanitizedDescription(blog.shortDescription),
-              }}
-            />
-          </div>
-        </Link>
-      );
+      return <BlogItem key={blog.id} blog={blog} />;
     })
   ) : (
     <div className="col-span-full py-10">
